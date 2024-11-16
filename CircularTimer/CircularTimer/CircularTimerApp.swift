a4aed4b6491f1519088cc543629325a31f0f7835
Gardeniaa101 <onejwon33@naver.com>
2024-11-16 17:02:04 +0900

    feat: CircularTimer 구현

---
+++ CircularTimer/CircularTimer/CircularTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularTimerApp.swift
+//  CircularTimer
+//
+//  Created by HanJW on 11/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CircularTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
